981d2565b1d1a59d1faea7926c7672e6a6dcd3d1
atifShaikh0 <maatif881@gamil.com>
2025-02-27 22:42:23 +0530

    managing member(admistrator can delete members etc)

--- src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
+++ src/app/(dashboard)/workspaces/[workspaceId]/page.tsx
@@ -1,14 +1,13 @@
 import { getCurrent } from "@/features/auth/quries";
 import { redirect } from "next/navigation";
 
-const WorkspaceIdPage = async () => {
+const WorkspaceIdPage = async ({}) => {
   const user = await getCurrent();
   if (!user) redirect("/sign-in");
 
   return (
     <div>
-      WorkspaceId
-      <div></div>
+      <div>Workspace id</div>
     </div>
   );
 };
