dbe62b1c1c80fadf45561d6813bd198b73673002
atifShaikh0 <maatif881@gamil.com>
2025-04-27 19:28:52 +0530

    calendar Page Done!

--- src/features/tasks/components/task-view-switcher.tsx
+++ src/features/tasks/components/task-view-switcher.tsx
@@ -17,6 +17,7 @@ import { columns } from "./columns";
 import { DataKanban } from "./data-kanban";
 import { TaskStatus } from "../types";
 import { useBulkUpdateTasks } from "../api/use-bulk-update-tasks";
+import { DataCalendar } from "./data-calendar";
 
 const TaskViewSwitcher = () => {
   const [{ status, assigneeId, projectId, dueDate }] = useTaskFilters();
@@ -88,8 +89,8 @@ const TaskViewSwitcher = () => {
                 data={tasks?.documents ?? []}
               />
             </TabsContent>
-            <TabsContent value="calendar" className="mt-0">
-              {JSON.stringify(tasks)}
+            <TabsContent value="calendar" className="mt-0 h-full pb-4">
+              <DataCalendar data={tasks?.documents ?? []} />
             </TabsContent>
           </>
         )}
